0e4c83c2e8ac38c191782d5e6db18f2ba5b738e8
liuyunc <liuyuno.12@gmail.com>
2017-07-04 20:05:18 +0800

    ui更新边框图

--- app/src/main/java/com/wx/android/fragment/ThirdPartyFragment.java
+++ app/src/main/java/com/wx/android/fragment/ThirdPartyFragment.java
@@ -1,12 +1,11 @@
 package com.wx.android.fragment;
 
-import android.graphics.Color;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.TextView;
 
+import com.wx.android.R;
 import com.wx.android.base.BaseFragment;
+import com.wx.android.fx.RingView;
 
 /**
  * 作者：尚硅谷-杨光福 on 2016/7/21 19:27
@@ -18,22 +17,27 @@ public class ThirdPartyFragment extends BaseFragment {
 
 
     private static final String TAG = ThirdPartyFragment.class.getSimpleName();//"CommonFrameFragment"
-    private TextView textView;
+    RingView mringView;
 
     @Override
     protected View initView() {
         Log.e(TAG,"第三方Fragment页面被初始化了...");
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.activity_heart,null);
+        mringView =(RingView) view.findViewById(R.id.ringView);
+        view.findViewById(R.id.startHeartBeatTest).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mringView.startAnim();
+            }
+        });
+
+        return view;
     }
 
     @Override
     protected void initData() {
         super.initData();
         Log.e(TAG, "第三方Fragment数据被初始化了...");
-        textView.setText("第三方页面");
+
     }
 }
